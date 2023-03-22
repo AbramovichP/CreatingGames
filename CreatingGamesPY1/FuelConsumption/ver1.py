d1671089f8c2b697a4a9fa82c835a6ab37d5635d
Petr_Abramovich <malyarpetr@gmail.com>
2023-03-22 22:19:21 +0300

    Программа готова

--- CreatingGamesPY1/FuelConsumption/ver1.py
+++ CreatingGamesPY1/FuelConsumption/ver1.py
@@ -4,9 +4,12 @@ price_litr = 0 # Цена за литр
 all_rashod = 0 # Всего литров на поездку
 all_price = 0 # Общая стоимость бензина
 
-rashod_na_100_km = int(input("Введите расход топлива автомобиля на 100 км.: "))
-all_km = int(input("Введите сколько километров собираетесь проехать: "))
-price_litr = int(input("Сколько стоит литр беньзина? "))
+rashod_na_100_km = float(input("Введите расход топлива автомобиля на 100 км.: "))
+all_km = float(input("Введите сколько километров собираетесь проехать: "))
+price_litr = float(input("Сколько стоит литр бензина? "))
 
 all_rashod = all_km / 100 * rashod_na_100_km
 all_price = all_rashod * price_litr
+
+print('Всего затратите топлива:', int(all_rashod * 100) / 100, 'л.')
+print('Общая цена:', int(all_price * 100) / 100, 'руб.')
